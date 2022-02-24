a269494ec2e9fbaff22011a3afcfbd44e3c881fd
Ryan Flitcroft <ryanflitcroft@ymail.com>
2022-02-24 10:10:16 -0800

    Function written for carMakeTotals for VictoryBar component data.

--- src/App.js
+++ src/App.js
@@ -1,54 +1,60 @@
 import * as React from 'react';
 import { DataGrid } from '@mui/x-data-grid';
+import { VictoryGroup, VictoryChart, VictoryBar, VictoryAxis } from 'victory';
 
-import data from './data.json';
-import { generateColumns } from './data-utils';
+import data from './data';
+import { generateColumns,
+  carMakeTotals } from './data-utils';
 
-const colData = generateColumns(data);
-console.log('colData', colData);
+const columnData = generateColumns(data);
+// console.log('colData', colData);
 
 export default function App() {
-  // const columns = [
-  //   { field: 'id', headerName: 'ID', width: 70 },
-  //   { field: 'firstName', headerName: 'First name', width: 130 },
-  //   { field: 'lastName', headerName: 'Last name', width: 130 },
-  //   {
-  //     field: 'age',
-  //     headerName: 'Age',
-  //     type: 'number',
-  //     width: 90,
-  //   },
-  //   {
-  //     field: 'fullName',
-  //     headerName: 'Full name',
-  //     description: 'This column has a value getter and is not sortable.',
-  //     sortable: false,
-  //     width: 160,
-  //     valueGetter: (params) =>
-  //       `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-  //   },
+
+  // const driverData = [
+  //   { quarter: 1, earnings: 13000 },
+  //   { quarter: 2, earnings: 16500 },
+  //   { quarter: 3, earnings: 14250 },
+  //   { quarter: 4, earnings: 19000 }
   // ];
   
-  const rows = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-  ];
+
   return (
-    <div style={{ height: 400, width: '100%' }}>
-      <DataGrid
-        rows={rows}
-        columns={colData}
-        pageSize={5}
-        rowsPerPageOptions={[5]}
-        checkboxSelection
-      />
-    </div>
+    <>
+      <div style={{ height: 500, width: '100%' }}>
+        <DataGrid
+          rows={data}
+          columns={columnData}
+          pageSize={10}
+          rowsPerPageOptions={[10]}
+          checkboxSelection
+        />
+      </div>
+
+
+      <VictoryChart>
+        <VictoryGroup>
+          <VictoryBar
+            data={carMakeTotals(data)}
+            height={300}
+            // style={{
+            //   labels: {
+            //     fontSize: 2
+            //   }
+            // }}
+          />
+        </VictoryGroup>
+        <VictoryAxis
+          style={{ tickLabels: { angle :90, fontSize: 5, textAnchor: 'start' } }}
+        />
+        <VictoryAxis
+          dependentAxis
+          tickValues={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}
+          style={{ tickLabels: { fontSize: 10 } }}
+        />
+        
+      </VictoryChart>
+
+    </>
   );
 }
--- src/data-utils.js
+++ src/data-utils.js
@@ -1,6 +1,7 @@
+import { AssignmentReturnedTwoTone } from '@mui/icons-material';
+
 export function generateColumns(arr) {
   const keys = Object.keys(arr[0]);
-  console.log(keys);
 
   const columns = keys.map((key) => {
     return {
@@ -10,4 +11,65 @@ export function generateColumns(arr) {
   });
 
   return columns;
+}
+
+export function average(arr) {
+  {/* average age for each gender */}
+  {/* {
+          m: 25,
+          f: 33,
+          x: 22
+        } */}
+  const allAges = arr.reduce((acc, curr) => {
+    acc[curr.gender]
+      ? acc[curr.gender].push(curr.age)
+      : acc[curr.gender] = [curr.age];
+
+    return acc;
+  }, {});
+
+
+  const avgAge = Object.entries(allAges)
+    .reduce((acc, curr) => {
+      const totalAge = curr[1].reduce((acc, curr) => {
+        return acc + curr;
+      }, 0);
+      const averageAge = totalAge / curr[1].length;
+      acc[curr[0]] = averageAge;
+
+      return acc;
+    }, {});
+  // console.log(avgAge);
+  return avgAge;
+}
+
+export function carMakeTotals(arr) {
+  // how many objects in arr have diff values for prop car_make
+  // [
+  //   { x: 'Ford', y: 10 },
+  //   { x: 'Kia', y: 3 },
+  //   { x: 'Honda', y: 7}
+  // ]
+  // Array of objects with key/value make/total
+    // {
+    //   ford: 8,
+    //   kia: 3,
+    //   toyota: 9
+    // }
+  const makes = arr.reduce((acc, curr) => {
+    acc[curr.car_make]
+      ? acc[curr.car_make]++
+      : acc[curr.car_make] = 1;
+    return acc;
+  }, {});
+
+  const entries = Object.entries(makes)
+    .reduce((acc, curr) => {
+      acc.push({ x: curr[0],
+        y: curr[1] });
+
+      return acc;
+    }, []);
+    
+  return entries;
 }
