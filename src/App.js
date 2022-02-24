f4c929926c53bec256cb2e1d94dcd944b7b07beb
Ryan Flitcroft <ryanflitcroft@ymail.com>
2022-02-24 11:20:13 -0800

    passing test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
+import './App.css';
 import * as React from 'react';
 import { DataGrid } from '@mui/x-data-grid';
 import { VictoryGroup, VictoryChart, VictoryBar, VictoryAxis, VictoryPie } from 'victory';
-
 import data from './data';
 import { generateColumns,
   carMakeTotals, averageAgeByGender } from './data-utils';
@@ -15,6 +15,7 @@ export default function App() {
 
   return (
     <>
+      <h2>Participant data</h2>
       <div style={{ height: 500, width: '100%' }}>
         <DataGrid
           rows={data}
@@ -25,11 +26,17 @@ export default function App() {
         />
       </div>
 
+      <h2>Vehicle make ownership of data participants</h2>
+
       <VictoryChart>
         <VictoryGroup>
           <VictoryBar
             data={carMakeTotalsData}
             height={300}
+            animate={{
+              duration: 2000,
+              onLoad: { duration: 2000 }
+            }}
           />
         </VictoryGroup>
         <VictoryAxis
@@ -42,11 +49,12 @@ export default function App() {
         />
       </VictoryChart>
 
-
+      <h2>Average age of data participants by gender</h2>
       <VictoryPie
         data={averageAgeByGenderData}
         height={300}
         innerRadius={50}
+        colorScale={['#388659', '#52AA5E', '#52AA8A', '#3AAED8', '#2BD9FE', '#F2C14E', '#F78154', '#B4436C']}
       />
     </>
   );
